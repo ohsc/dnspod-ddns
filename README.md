0c09a09ca41fe027898bd85efebc234448894e8a
imchao <shen218@gmail.com>
2012-11-15 10:43:22 +0800

    add dnspod-ddns 0.1

---
+++ README.md
@@ -0,0 +1,31 @@
+# dnspod-ddns
+
+dnspod-ddns is a ddns updating tool of [DNSPod](http://www.dnspod.cn).
+It was originally designed for use with [node.js](http://nodejs.org).
+
+
+## Download
+
+You can install using Node Package Manager (npm):
+
+```sh
+npm install -g dnspod-ddns
+```
+
+## Usage
+
+dnspod-ddns is a command line tool. It will update your dns record after you execute the following. You can add it to crontab for continuously updating.
+
+```sh
+dnspod-ddns --dns-domain yourdomain.com --dns-host yourhost --user youremail --passwd yourpassword
+```
+
+Otherwise, you can run it in daemon mode.
+
+```sh
+dnspod-ddns --dns-domain yourdomain.com --dns-host yourhost --user youremail --passwd yourpassword --daemon
+```
+
+## License
+
+Copyright (c) 2012 Chao Shen. This software is licensed under the BSD License.
---
+++ bin/cli.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+var fs = require('fs'),
+    program = require('commander'),
+    ddns = require('../'),
+    options;
+
+
+program
+    .version('0.0.1')
+    .usage('[options] [value ...]')
+    .option('-d, --dns-domain <domain>', 'Set domain name(*required)')
+    .option('-H, --dns-host <host>', 'Set host name(*required)')
+    .option('-u, --user <user>', 'Set user name(*required)')
+    .option('-p, --passwd <password>', 'Set password(*required)')
+    .option('-D, --daemon', 'Start as daemon mode')
+    .parse(process.argv);
+
+if (!program.dnsDomain) {
+    console.log('--dns-domain is required.');
+    process.exit(-1);
+} else if (!program.dnsHost) {
+    console.log('--dns-host is required.');
+    process.exit(-1);
+} else if (!program.user) {
+    console.log('--user is required.');
+    process.exit(-1);
+} else if (!program.passwd) {
+    console.log('--passwd is required.');
+    process.exit(-1);
+} else {
+    options = {
+        domain: program.dnsDomain,
+        sub_domain: program.dnsHost,
+        login_email: program.user,
+        login_password: program.passwd,
+        is_daemon: program.daemon
+    };
+    console.log('received args %j', options);
+    ddns.run(options);
+}
+
+
+
