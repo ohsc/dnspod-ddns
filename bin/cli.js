ac19486d5866800daf28eac1c03d7640ed839e37
liu lei <leolovenet@gmail.com>
2015-08-10 18:07:17 +0800

    support login_token.

--- bin/cli.js
+++ bin/cli.js
@@ -14,6 +14,7 @@ program
     .option('-H, --dns-host <host>', 'Set host name(*required)')
     .option('-u, --user <user>', 'Set user name(*required)')
     .option('-p, --passwd <password>', 'Set password(*required)')
+    .option('-t, --login-token <token>','Set Login Token(*required)')
     .option('-D, --daemon', 'Start as daemon mode')
     .option('-i, --interface <interface>', 'Use IP address of specified network interface')
     .parse(process.argv);
@@ -24,23 +25,22 @@ if (!program.dnsDomain) {
 } else if (!program.dnsHost) {
     console.log('--dns-host is required.');
     process.exit(-1);
-} else if (!program.user) {
-    console.log('--user is required.');
-    process.exit(-1);
-} else if (!program.passwd) {
-    console.log('--passwd is required.');
-    process.exit(-1);
 } else {
-    options = {
+     options = {
         domain: program.dnsDomain,
         sub_domain: program.dnsHost,
-        login_email: program.user,
-        login_password: program.passwd,
         is_daemon: program.daemon,
         interface_name: program.interface
     };
-    ddns.run(options);
+
+     if (program.loginToken) {
+         options.login_token = program.loginToken;
+     } else if (program.user && program.passwd) {
+       options.login_email = program.user;
+       options.login_password = program.passwd;
+     } else {
+        console.log( '--login-token or --user with --passwd is required.');
+        process.exit(-1);
+     }
+      ddns.run(options);
 }
-
-
-
